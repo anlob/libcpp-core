3827bb2e0624a267627b84ed537cf0d57370b70a
anlob <andreas.lobbes@gmail.com>
2018-06-16 12:42:48 +0200

    copyright info in doxygen hdr reworked

--- src/FD.cpp
+++ src/FD.cpp
@@ -4,6 +4,7 @@
  * \author Andreas Lobbes, andreas.lobbes@gmail.com
  * \date 2016-2018
  * \copyright GPLV3 and above + GCC RTL Exception 3.1
+ * \see LICENSES at top level directory
  * \see http://www.gnu.org/licenses
  */
 #include <cerrno>
